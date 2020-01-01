f5026bb7cfac3df2442117d9b09e5907a5507ca0
DJ Bostock <douglasjbostock@gmail.com>
2020-01-01 12:00:00 -0600

    reboot

--- includes/footer.php
+++
@@ -1,1 +0,0 @@
-<p>&copy; DJ Bostock <span id="currentYear"></span></p>
--- quiz2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include './questionBank.php';
-
-$number = random_int(0, 3);
-$question = $questionBank[$number];
-
-echo json_encode($question);
