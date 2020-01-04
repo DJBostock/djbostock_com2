e730830e8864dab0aa1b3d138061405b3773437e
DJ Bostock <douglasjbostock@gmail.com>
2020-01-04 12:00:00 -0600

    cumulative updates

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
+<?php 
+
+$title = "DJ Bostock";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DJ Bostock</title>
+    <title><?= $title; ?></title>
 
     <link rel="stylesheet" href="css/style.css">
 </head>
