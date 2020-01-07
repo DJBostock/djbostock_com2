3cf12cdfa489826f73b1117449afa646851f5777
DJ Bostock <douglasjbostock@gmail.com>
2020-01-07 12:00:00 -0600

    cumulative updates

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ $title = "DJ Bostock";
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
+    <h1>DJ Bostock</h1>
     <script src="js/script.js"></script>
 </body>
 </html>
