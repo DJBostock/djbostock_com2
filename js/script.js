a480df134c68b2c86e855eec3c31b70798826307
DJ Bostock <douglasjbostock@gmail.com>
2020-01-29 12:00:00 -0600

    cumulative updates

--- js/script.js
+++ js/script.js
@@ -1,14 +1,1 @@
-const projectCards = document.querySelectorAll(".projectContainer div.card");
-
-function removeActiveClasses() {
-  projectCards.forEach((card) => {
-    card.classList.remove("active");
-  });
-}
-
-projectCards.forEach((card) => {
-  card.addEventListener("click", () => {
-    removeActiveClasses();
-    card.classList.add("active");
-  });
-});
+console.log("test");
