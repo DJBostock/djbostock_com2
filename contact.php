325c619e76494764f3a88bdb76bd569a2ed2e317
DJ Bostock <douglasjbostock@gmail.com>
2020-02-08 12:00:00 -0600

    cumulative updates

---
+++ contact.php
@@ -0,0 +1,37 @@
+<?php 
+
+$title = "DJ Bostock - Contact Me";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="DJ Bostock">
+
+    <title><?= $title; ?></title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <h1>DJ Bostock</h1>
+        <img src="images/profile-round.png" alt="cartoon headshot of the author" class="hero">
+    </header>
+
+    <?php include './includes/nav.php'; ?>
+
+    <main>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam cum autem ducimus nam, in doloremque consectetur inventore quae nesciunt commodi dignissimos porro error dolorum quisquam repellendus quod officia illo ut.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam cum autem ducimus nam, in doloremque consectetur inventore quae nesciunt commodi dignissimos porro error dolorum quisquam repellendus quod officia illo ut.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam cum autem ducimus nam, in doloremque consectetur inventore quae nesciunt commodi dignissimos porro error dolorum quisquam repellendus quod officia illo ut.</p>
+    </main>
+    <footer>
+        <p>&copy; DJ Bostock</p>
+    </footer>
+    <script src="js/script.js"></script>
+</body>
+</html>
