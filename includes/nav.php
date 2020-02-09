808feaf77517517f6a4dd9c8b1054367b71ca5d2
DJ Bostock <douglasjbostock@gmail.com>
2020-02-09 12:00:00 -0600

    cumulative updates

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,7 @@
 <nav class="mainNav">
     <ul>
         <li><a href="./index.php">Home</a></li>
+        <li><a href="./contact.php">Contact Me</a></li>
         <li><a href="./projects.php">Projects</a></li>
     </ul>
 </nav>
