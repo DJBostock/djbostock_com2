425c175c4e7bb730b1869b02a1b0676baa4d1451
DJ Bostock <douglasjbostock@gmail.com>
2023-12-14 07:05:27 -0600

    start contact page

---
+++ contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Contact - DJ Bostock</title>
+
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <nav>
+        <span class="logo">DJ Bostock</span>
+    </nav>
+    <header>
+        <h1>Contact Me</h1>
+    </header>
+    <main>&nbsp;</main>
+    <footer>&copy; <span id="currentYear"></span></footer>
+    <script src="./js/script.js"></script>
+</body>
+</html>
