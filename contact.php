6886f8281f276bf924e582568df86147c6c6d974
DJ Bostock <douglasjbostock@gmail.com>
2023-12-14 09:58:27 -0600

    additional PHP variable test

--- contact.php
+++ contact.php
@@ -1,10 +1,16 @@
+<?php
+
+$title = "Contact - DJ Bostock";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Contact - DJ Bostock</title>
+    <title><?= $title; ?></title>
 
     <link rel="stylesheet" href="./css/style.css">
 </head>
