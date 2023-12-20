45a80b7a32e8a35c6adfd8967c44661c2dfdb8a5
DJ Bostock <douglasjbostock@gmail.com>
2023-12-20 12:00:00 -0600

    add author meta tag

--- contact.php
+++ contact.php
@@ -16,8 +16,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     mail($to="douglasjbostock@gmail.com", $subject=$userSubject, $message=$userMessage, $from=$userFrom);
 }
 
-
-
 ?>
 
 <!DOCTYPE html>
@@ -25,6 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="DJ Bostock">
 
     <title><?= $title; ?></title>
 
--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $title = "DJ Bostock";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="DJ Bostock">
 
     <title><?= $title; ?></title>
 
