2bc2c86b757ae294a654c059fc36c850a6b8e567
DJ Bostock <douglasjbostock@gmail.com>
2023-12-21 12:00:00 -0600

    add description meta tag

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $title = "DJ Bostock";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="DJ Bostock">
+    <meta name="description" content="Homepage of DJ Bostock">
 
     <title><?= $title; ?></title>
 
