23be22775627d18efaad208e0d5205ca6ce9e5c5
DJ Bostock <douglasjbostock@gmail.com>
2023-12-23 12:00:00 -0600

    start expanding cards project page

---
+++ projects/expanding-cards/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$title = "Expanding Cards";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("../../includes/head.php"); ?>
+</head>
+<body>
+    <nav>
+        <span class="logo">DJ Bostock</span>
+        <span>
+            <a href="index.php">Home</a>
+            <a href="contact.php">Contact</a>
+        </span>
+    </nav>
+    <header>
+        <h1><?= $title; ?></h1>
+    </header>
+    <main>
+        &nbsp;
+    </main>
+    <footer>&copy; <span id="currentYear"></span></footer>
+    <script src="../../js/script.js"></script>
+</body>
+</html>
