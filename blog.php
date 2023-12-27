203a3fe5b6a99552dea718829ff51146f3ebd026
DJ Bostock <douglasjbostock@gmail.com>
2023-12-27 12:00:00 -0600

    test database connection

--- blog.php
+++ blog.php
@@ -1,13 +1,24 @@
+<?php $title = "Blog - DJ Bostock"; ?>
+<?php include("includes/db-credentials.php"); ?>
 <?php
-
-$title = "Blog - DJ Bostock";
-
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    if (mysqli_connect_error()) {
+        echo mysqli_connect_error();
+        exit;
+    }
+    $sql = "SELECT * FROM article ORDER BY published_at;";
+    $results = mysqli_query($conn, $sql);
+    if ($results === false) {
+        echo mysqli_error($conn);
+    } else {
+        $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include("./includes/head.php"); ?>
+    <?php include("includes/head.php"); ?>
 </head>
 <body>
     <nav>
@@ -16,7 +27,9 @@ $title = "Blog - DJ Bostock";
     <header>
         <h1>Blog</h1>
     </header>
-    <main>&nbsp;</main>
+    <main>
+        <?php var_dump($articles); ?>
+    </main>
     <footer>&copy; <span id="currentYear"></span></footer>
     <script src="./js/script.js"></script>
 </body>
