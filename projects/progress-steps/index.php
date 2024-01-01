2b609cf43c41e1346220dbd3ec307c87bb696176
DJ Bostock <douglasjbostock@gmail.com>
2024-01-01 12:00:00 -0600

    add starter code for progress steps project

---
+++ projects/progress-steps/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$title = "Progress Steps";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("../../includes/head.php"); ?>
+    <link rel="stylesheet" href="./project-style.css">
+</head>
+<body>
+    <nav>
+        <?php include("../../includes/nav.php"); ?>
+    </nav>
+    <header>
+        <h1><?= $title; ?></h1>
+        <p>Adapted from Brad Traversy's course on Udemy.com <a href="https://www.udemy.com/share/103Pje3@vWss779JzZo8tc0tYJXHMFGHv7t82AdaVag7W4uKhDDTpRbEgZ-2HK1vbSc6-tphwg==/">50 Projects In 50 Days - HTML, CSS & JavaScript</a></p>
+    </header>
+    <main>
+        <div class="container">&nbsp;</div>
+    </main>
+    <footer>&copy; <span id="currentYear"></span></footer>
+    <script src="../../js/script.js"></script>
+    <script src="./project-script.js"></script>
+</body>
+</html>
