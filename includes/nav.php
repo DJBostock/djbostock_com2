f1f6d03bd3db4e6c7edb0bf61ee72d97c92ae974
DJ Bostock <douglasjbostock@gmail.com>
2025-07-22 17:47:08 -0500

    reset homepage content

--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,3 @@
 <ul>
-    <li><a href="#about-me">About Me</a></li>
-    <li><a href="#portfolio">Portfolio</a></li>
-    <li><a href="#contact-me">Contact Me</a></li>
+    <li><a href="index.php">Home</a></li>
 </ul>
--- index.php
+++ index.php
@@ -29,34 +29,6 @@
         <nav>
             <?php include './includes/nav.php'; ?>
         </nav>
-        <section>
-            <h2 id="#about-me">About Me</h2>
-            <p>Early in my working life I spent several years working on the production floor of a factory. One of the
-                things that I learned there that is most applicable to my career in tech was a lot of practice with
-                troubleshooting. Things would go wrong from time to time and often I would be the first person to try to
-                figure out the what and why, and of course if I couldn't fix it then I would have to describe the issue
-                to the maintanence person who would be next in line to diagnose the issue.</p>
-            <p>My first tech related position was (among other things) doing application support in a call center. In
-                this role one of my duties was to identify bugs reported by users of the software that the company
-                wrote, reproduce them, and send them up to the developers. Once the developers had a solution I would
-                then work with the users to confirm that the issue was resolved.</p>
-            <p>My next tech related position was more what a person probably imagines when they think of a tech support
-                job. I worked for a government contractor on a remote help desk. I supported just about every device and
-                piece of software under the sun. Sometimes it seemed like if it used electricity and a government worker
-                used it, then they would call me for help with it. This job definitely expanded my ability to look up
-                most any issue to help the callers. I got a lot of experience with gathering the right information to
-                understand what the issue was and how to help them get past it.</p>
-        </section>
-        <section>
-            <h2 id="#portfolio">Portfolio</h2>
-            <p>Under construction</p>
-        </section>
-        <section>
-            <h2 id="#contact-me">Contact Me</h2>
-            <ul>
-                <li>You can email me at: <a href="mailto:douglasjbostock@gmail.com">douglasjbostock@gmail.com</a></li>
-            </ul>
-        </section>
     </main>
 </body>
 
