2921af5c7f1a5d427b99a6f5e7570cda0578ac45
DJ Bostock <douglasjbostock@gmail.com>
2025-07-24 10:00:17 -0500

    break out separate Portfolio page

--- includes/nav.php
+++ includes/nav.php
@@ -1,4 +1,5 @@
 <ul>
     <li><a href="index.php">Home</a></li>
     <li><a href="about.php">About Me</a></li>
+    <li><a href="portfolio.php">Portfolio</a></li>
 </ul>
---
+++ portfolio.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="personal website of DJ Bostock">
+    <meta name="author" content="DJ Bostock">
+
+    <title>DJ Bostock - Portfolio</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/script.js" defer></script>
+</head>
+
+<body>
+    <header>
+        <div>
+            <h1>DJ Bostock</h1>
+            <h2>IT & Web Enthusiast</h2>
+        </div>
+        <div>
+            <img src="images/happy-stick.jpg" alt="smiling stick figure">
+        </div>
+
+    </header>
+    <main>
+        <nav>
+            <?php include './includes/nav.php'; ?>
+        </nav>
+        <section>
+            <h2>Portfolio</h2>
+            <p>Under construction</p>
+        </section>
+    </main>
+</body>
+
+</html>
