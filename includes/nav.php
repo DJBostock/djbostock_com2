60fe859b67a58471a38eebb0122773b2936514f5
DJ Bostock <douglasjbostock@gmail.com>
2025-07-31 08:55:32 -0500

    add home link to navbar

--- includes/nav.php
+++ includes/nav.php
@@ -7,6 +7,9 @@
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="about.php">About</a>
                 </li>
                 <li class="nav-item">
