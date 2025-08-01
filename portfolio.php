0fa4aae4ad709cea4f605e7cd30cd3ef6e79ee8a
DJ Bostock <douglasjbostock@gmail.com>
2025-08-01 09:24:04 -0500

    update portfolio listing

--- portfolio.php
+++ portfolio.php
@@ -21,6 +21,13 @@ $pageTitle = 'DJ Bostock';
             <div class="col-12 col-md-8">
                 <h1 class="mb-5">Portfolio</h1>
                 <div class="card mb-4 card-shadow">
+                    <div class="card-header">Temperature Converter</div>
+                    <div class="card-body">This is a Java program that prompts the user for a degree in either Celsius or Fahrenheit and converts it to the other.</div>
+                    <div class="card-footer">
+                        <a href="https://github.com/DJBostock/temperature-converter-Java" target="_blank">View on Github</a>
+                    </div>
+                </div>
+                <div class="card mb-4 card-shadow">
                     <div class="card-header">Addition Calculator</div>
                     <div class="card-body">This is a C# program that prompts the user for two numbers and then displays the sum of them.</div>
                     <div class="card-footer">
@@ -41,13 +48,13 @@ $pageTitle = 'DJ Bostock';
                         <a href="https://github.com/DJBostock/favorite-number" target="_blank">View on Github</a>
                     </div>
                 </div>
-                <div class="card mb-4 card-shadow">
+                <!-- <div class="card mb-4 card-shadow">
                     <div class="card-header">Hello World in Java</div>
                     <div class="card-body">This is a Hello World program written in Java</div>
                     <div class="card-footer">
                         <a href="https://github.com/DJBostock/hello-world-java" target="_blank">View on Github</a>
                     </div>
-                </div>
+                </div> -->
             </div>
             <div class="col-12 col-md-4">
                 <img src="./images/happy-stick.jpg" alt="happy stick person" class="hero-img">
