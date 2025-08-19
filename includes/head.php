0f305b4062549ef69aff8c7fd820c78f7eb3f8a2
DJ Bostock <douglasjbostock@gmail.com>
2025-08-19 14:31:11 -0500

    complete restart

--- includes/head.php
+++ includes/head.php
@@ -6,9 +6,5 @@
 
 <title><?= $pageTitle; ?></title>
 
-<!-- BOOTSTRAP CDN -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
 <!-- CUSTOM CSS -->
 <link rel="stylesheet" href="./css/style.css">
--- index.php
+++ index.php
@@ -18,24 +18,10 @@ $pageTitle = 'DJ Bostock';
         <?php include './includes/nav.php'; ?>
 
         <!-- MAIN -->
-        <div class="mt-4 p-5 rounded row">
-            <div class="col-12 col-md-8">
-                <h1 class="mb-5">DJ Bostock</h1>
-                <p>Hello, World! Welcome to my page.</p>
-                <p>I am a developer into turning ideas into functional code.</p>
-                <p>I am eager to join a team where I can contribute and grow my skills.</p>
-            </div>
-            <div class="col-12 col-md-4">
-                <img src="./images/happy-stick.jpg" alt="happy stick person" class="hero-img">
-            </div>
-        </div>
-        <div class="mt-4 p-5 rounded row">
-            <div class="col">
-                <button class="btn btn-primary btn-lg rounded" onclick="window.location.href='https://djbostock.com/portfolio.php'">View My Portfolio</button>
-            </div>
-        </div>
+        <div></div>
+
         <!-- FOOTER -->
-        <div class="ps-5 pe-5">
+        <div>
             <?php include './includes/footer.php'; ?>
             <script src="./js/script.js"></script>
         </div>
