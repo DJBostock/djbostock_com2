ae16086071071cdd28b04bd00cb86ea58c313507
DJ Bostock <douglasjbostock@gmail.com>
2025-08-24 12:54:15 -0500

    add description meta tag

--- includes/head.php
+++ includes/head.php
@@ -2,6 +2,7 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="author" content="DJ Bostock">
+<meta name="description" content="This is the portfolio website for DJ Bostock as a developer.">
 
 <link rel="icon" type="image/x-icon" href="./favicon.ico">
 
